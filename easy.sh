587fae9339fb3388543172c51f4f85d6311483d0
usernameisnotavailablehere <123851886+usernameisnotavailablehere@users.noreply.github.com>
2023-08-30 14:01:35 -0700

    Update easy.sh

--- easy.sh
+++ easy.sh
@@ -3,7 +3,7 @@ npm install
 npm run build
 
 pm2 delete Ludicrous
-pm2 start npm --name "Ludicrous" -- start
+pm2 start npm --name "Website for people who don't care about school" -- start
 nginx -s reload
 
 echo "done"
